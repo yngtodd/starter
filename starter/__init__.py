01db9be9496bb2f28aab5204cf40e09b3eb57e1e
drkostas <georgiou.kostas94@gmail.com>
2021-02-21 15:04:10 -0500

    Changed the package folder alias #2

---
+++ starter/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for Starter."""
+
+from starter.fancy_logger import ColorizedLogger
+from starter.timing_tools import timeit
+from starter.configuration import Configuration, validate_json_schema
+
+__author__ = "drkostas"
+__email__ = "georgiou.kostas94@gmail.com"
+__version__ = "0.2.0"
