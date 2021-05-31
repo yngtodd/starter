802080701e5c0cf7aa7393a2018742eb6982bd46
drkostas <georgiou.kostas94@gmail.com>
2021-05-31 15:16:09 -0400

    Imported cloudstore static version. #10

--- Starter/starter/__init__.py
+++ Starter/starter/__init__.py
@@ -4,6 +4,7 @@ from starter.fancy_logger import ColorizedLogger
 from starter.timing_tools import timeit
 from starter.profiling_funcs import profileit
 from starter.configuration import Configuration, validate_json_schema
+from starter.cloudstore import DropboxCloudstore
 
 __author__ = "drkostas"
 __email__ = "georgiou.kostas94@gmail.com"
